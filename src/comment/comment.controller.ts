075db2cd3ce75819b0d7ccc6af1d940dc2445f97
KimJaeHyeok <master_catch@naver.com>
2023-08-21 12:42:27 +0900

    fix:board,cmt 중간커밋

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+  } from '@nestjs/common';
+  import { CommentService } from './comment.service';
+  import { CreateCommentDto } from '../dto/comment/create-comment.dto';
+  import { DeleteCommentDto } from '../dto/comment/delete-comment.dto';
+  import { UpdateCommentDto } from '../dto/comment/update-comment.dto';
+  
+  @Controller('board')
+  export class BoardController {
+    constructor(private readonly CommentService: CommentService) {}
+  
+    @Get('/comments')
+    getcomments() {
+      return this.CommentService.getcomments();
+    }
+  
+    @Get('/comments/:id')
+    getArticleById(@Param('id') articleId: number) {
+      return this.CommentService.getArticleById(articleId);
+    }
+  
+    @Post('/comments')
+    createArticle(@Body() data: CreateCommentDto) {
+      return this.CommentService.createArticle(
+        data.comment,
+      );
+    }
+  
+    @Put('/comments/:id')
+    updateArticle(
+      @Param('id') articleId: number,
+      @Body() data: UpdateCommentDto,
+    ) {
+      return this.CommentService.updateArticle(
+        articleId,
+        data.comment,
+      );
+    }
+  
+    @Delete('/comments/:id')
+    deleteArticle(
+      @Param('id') articleId: number,
+      @Body() data: DeleteCommentDto,
+    ) {
+      return this.CommentService.deleteArticle(articleId, data.password);
+    }
+  }
 
-@Controller('comment')
-export class CommentController {}
