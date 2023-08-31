f1813290b3c56e5bbc13767ee519604fd8e0a78c
Kim Taejin <rlaxowls0504@gmail.com>
2023-08-31 22:36:40 +0900

    style: app.module 로거 미들웨어 주석 해제

--- src/app.module.ts
+++ src/app.module.ts
@@ -89,6 +89,6 @@ import { ChatModule } from './chat/chat.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
