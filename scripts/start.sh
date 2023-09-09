58bc1856ea13b8d5580c2e69bd95f7b3428f2743
Kim Taejin <rlaxowls0504@gmail.com>
2023-09-09 22:12:31 +0900

    chore: start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-APP_NAME="app"
+APP_NAME="apple_tree"
 
 TIME_NOW=$(date +%c)
 
 cd $PROJECT_ROOT
 
+npm i
 pm2 delete $APP_NAME
 pm2 start npm --name $APP_NAME -- start
 
