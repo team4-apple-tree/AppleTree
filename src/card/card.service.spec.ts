a2c6c483f2ce7369b2e0eaeca10c7ba24783b3a5
Kim Taejin <rlaxowls0504@gmail.com>
2023-09-09 14:42:10 +0900

    fix: card.service.spec.ts 수정

--- src/card/card.service.spec.ts
+++ src/card/card.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardService } from './card.service';
+import { CardModule } from './card.module';
 
 describe('CardService', () => {
   let service: CardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CardModule],
       providers: [CardService],
     }).compile();
 
